bad2eeb3a9fd271dcf507a6845e9e4d41f1c3fad
Çağkan Mert Öztaş <cmoztas2@gmail.com>
2021-02-03 00:06:19 +0300

    Add project files.

---
+++ MyList/MyList.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace MyList
+{
+    internal class MyList<T>
+    {
+        private T[] _items;
+
+        public MyList()
+        {
+            _items = new T[0];
+        }
+
+        public int Count => _items.Length;
+
+        public void Add(T item)
+        {
+            IncreaseArraySize();
+
+            _items[^1] = item;
+        }
+
+        private void IncreaseArraySize()
+        {
+            T[] tempList = _items;
+            _items = new T[tempList.Length + 1];
+
+            for (int i = 0; i < tempList.Length; i++)
+            {
+                _items[i] = tempList[i];
+            }
+        }
+
+        public T[] GetAll() => _items;
+    }
+}
---
+++ MyList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyList
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            MyList<string> mylist = new MyList<string>();
+
+            mylist.Add("Çağkan");
+            mylist.Add("Selin");
+            mylist.Add("Ece");
+
+            Console.WriteLine($"Dizinin şu anki boyutu: {mylist.Count}");
+
+            foreach (string item in mylist.GetAll())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\n--------------------\n");
+
+            mylist.Add("Timuçin");
+
+            Console.WriteLine($"Dizinin şu anki boyutu: {mylist.Count}");
+
+
+            foreach (string item in mylist.GetAll())
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
